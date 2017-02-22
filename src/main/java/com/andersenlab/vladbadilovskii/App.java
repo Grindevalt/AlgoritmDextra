bae6c37a0f983a21b350b4ac2b312ab0924b390d
Grindevalt <v.badilovschi@andersenlab.com>
2017-02-22 13:30:00 +0200

    sdasd

--- src/main/java/com/andersenlab/vladbadilovskii/App.java
+++ src/main/java/com/andersenlab/vladbadilovskii/App.java
@@ -4,4 +4,7 @@ package com.andersenlab.vladbadilovskii;
  * Created by Vlad Badilovskii on 22.02.2017.
  */
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
